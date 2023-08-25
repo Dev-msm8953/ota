89a99ac01c6c835c74e25ca999f5021678a80792
TogoFire <togofire@mailfence.com>
2023-08-24 21:59:47 -0300

    createjson.sh: set daisy volt 2.7
    curl -o createjson.sh https://raw.githubusercontent.com/Dev-msm8953/ota/voltage-daisy/initial_support/createjson.sh && chmod +x createjson.sh && ./createjson.sh
    
    Change-Id: 12a8339cc9344ac1ec45d5b234d7014679772727
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- initial_support/createjson.sh
+++ initial_support/createjson.sh
@@ -7,7 +7,7 @@ maintainer="TogoFire"
 oem="Xiaomi"
 device="daisy"
 devicename="Mi A2 Lite"
-zip="voltage-2.6-daisy-20230714-1715-OFFICIAL.zip"
+zip="voltage-2.7-daisy-20230824-2254-OFFICIAL.zip"
 
 #don't modify from here
 script_path="`dirname \"$0\"`"
